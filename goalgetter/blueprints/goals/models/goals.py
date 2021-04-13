74fe04dacf2c6ae4afce8507b0f98517690fd43e
Shing Tze Woo <shingtzewoo@gmail.com>
2021-04-13 15:48:20 -0700

    Corrections made to the model

--- goalgetter/blueprints/goals/models/goals.py
+++ goalgetter/blueprints/goals/models/goals.py
@@ -1,15 +1,14 @@
 from goalgetter.extensions import db
-from goalgetter.blueprints.user.models import User
 from goalgetter.blueprints.goals.models.milestones import Milestone
 
-class Goal(db.model):
+class Goal(db.Model):
 
     __tablename__ = "goals"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False, server_default='')
 
     # M:1 relationship with the users table, https://docs.sqlalchemy.org/en/14/core/constraints.html#sqlalchemy.schema.ForeignKey and https://docs.sqlalchemy.org/en/14/orm/cascades.html#passive-deletes
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', onupdate='CASCADE', ondelete='CASCADE'), index=True, nullable=False))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', onupdate='CASCADE', ondelete='CASCADE'), index=True, nullable=False)
     # 1:M relationship with milestones
     milestone = db.relationship(Milestone, uselist=True, backref='milestones', passive_deletes=True)
 
--- goalgetter/blueprints/goals/models/milestones.py
+++ goalgetter/blueprints/goals/models/milestones.py
@@ -1,7 +1,6 @@
 from goalgetter.extensions import db
-from goalgetter.blueprints.goals.models.goals import Goal
 
-class Milestone(db.model):
+class Milestone(db.Model):
 
     __tablename__ = "milestones"
     id = db.Column(db.Integer, primary_key=True)
@@ -9,11 +8,10 @@ class Milestone(db.model):
 
     # milestones can have many tasks, however, it is better to store these tasks in an array column instead of by itself since they will not be unique
     # https://stackoverflow.com/questions/52925453/how-to-set-an-array-column-with-an-empty-array-as-default-in-sqlalchemy-postgr
-    tasks = db.Column(db.Array(db.String), server_default='[]')
+    tasks = db.Column(db.ARRAY(db.String), server_default='{}')
 
     # M:1 relationship with the goals table, https://docs.sqlalchemy.org/en/14/core/constraints.html#sqlalchemy.schema.ForeignKey and https://docs.sqlalchemy.org/en/14/orm/cascades.html#passive-deletes
-    goal = db.Column(db.Integer, db.ForeignKey('goals.id', onupdate='CASCADE', ondelete='CASCADE'), index=True, nullable=False))
+    goal = db.Column(db.Integer, db.ForeignKey('goals.id', onupdate='CASCADE', ondelete='CASCADE'), index=True, nullable=False)
 
     def __init__(self, **kwargs):
         super(Goal, self).__init__(**kwargs)
-    
