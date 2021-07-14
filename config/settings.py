1d8246d6bcdc75698990341c5e09cb6fda6cdf09
Shing Tze Woo <shingtzewoo@gmail.com>
2021-07-13 22:34:19 -0700

    changes to config file

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,8 @@ SECRET_KEY = "b'}\x15\xfa:4A_\x85\x02\xcc\xbfN1\xf9\xe3/\xfd\xb25\xdeK\x97\x84\x
 # SQLAlchemy.
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-SQLALCHEMY_DATABASE_URI = DATABASE_URL
+SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+# https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # User.
