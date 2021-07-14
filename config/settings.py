73a8901a9517ea6c897a1a6ecd9dbc9d306211f1
Shing Tze Woo <shingtzewoo@gmail.com>
2021-07-13 21:55:40 -0700

    changes to config file

--- config/settings.py
+++ config/settings.py
@@ -8,8 +8,7 @@ SERVER_NAME = 'gentle-bayou-82963.herokuapp.com'
 SECRET_KEY = "b'}\x15\xfa:4A_\x85\x02\xcc\xbfN1\xf9\xe3/\xfd\xb25\xdeK\x97\x84\xc2'"
 
 # SQLAlchemy.
-# db_uri = 'postgresql://goalgetter:devpassword@postgres:5432/goalgetter'
-db_uri = os.environ['DATABASE_URL']
+db_uri = os.environ['postgresql://goalgetter:devpassword@postgres:5432/goalgetter']
 conn = psycopg2.connect(db_uri, sslmode='require')
 SQLALCHEMY_DATABASE_URI = db_uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
