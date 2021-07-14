ba0d56e1ca417b9e26007b2c6496799fa0043e30
Shing Tze Woo <shingtzewoo@gmail.com>
2021-07-13 21:51:20 -0700

    changes to config file

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,16 @@
 from datetime import timedelta
+import os
+import psycopg2
+
 DEBUG = True
 
 SERVER_NAME = 'gentle-bayou-82963.herokuapp.com'
 SECRET_KEY = "b'}\x15\xfa:4A_\x85\x02\xcc\xbfN1\xf9\xe3/\xfd\xb25\xdeK\x97\x84\xc2'"
 
 # SQLAlchemy.
-db_uri = 'postgresql://goalgetter:devpassword@postgres:5432/goalgetter'
+# db_uri = 'postgresql://goalgetter:devpassword@postgres:5432/goalgetter'
+db_uri = os.environ['DATABASE_URL']
+conn = psycopg2.connect(db_uri, sslmode='require')
 SQLALCHEMY_DATABASE_URI = db_uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
