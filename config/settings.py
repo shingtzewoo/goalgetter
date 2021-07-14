d67b63f9f9a8f130037a2968cadd77a69caa4036
Shing Tze Woo <shingtzewoo@gmail.com>
2021-07-13 22:11:11 -0700

    modifications to heroku postgresql

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = "b'}\x15\xfa:4A_\x85\x02\xcc\xbfN1\xf9\xe3/\xfd\xb25\xdeK\x97\x84\x
 # SQLAlchemy.
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-SQLALCHEMY_DATABASE_URI = db_uri
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # User.
